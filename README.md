88aa6e2f380cdb57fe45966235255e789b2b4040
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-03-04 11:43:07 -0600

    added license link and sections function

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 
-  ## Title
-  project
   ![GitHub license](https://img.shields.io/badge/license-MIT%20License-blue.svg)
+  ## Title
+  README GENERATOR
   
   ## Description
-  des
+  This project generates a  professional readme .
 
  
   ## Table of Contents
-  [Installation] (#installation)
+  *[Installation] (#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Tests](#test)
+  *[Questions](#questions)
 
-   
-  # username
-  # email
+
+  ## License
+  This project is licenced under the MIT License License.
+  ![GitHub license](https://img.shields.io/badge/license-MIT%20License-blue.svg)
+
+
+  ## Questions
+  If you have any question about the repo you can contact me via jonathan.hanahan@gmail.com.
+  You can also find more of my work at[@hanahanj](https://github.com/@hanahanj).
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,32 +14,51 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== null){
+    return `*[License](#license)`
+  }
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
 // add to table of contents 
-
+if (license !== null){
+  return `## License
+  This project is licenced under the ${license} License.`
+}
+}
 
 
 //Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
+  ${renderLicenseBadge(data.license)}
   ## Title
   ${data.title}
-  ${renderLicenseBadge(data.license)}
   
   ## Description
   ${data.description}
 
  
   ## Table of Contents
-  [Installation] (#installation)
+  *[Installation] (#installation)
+  *[Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  *[Contributing](#contributing)
+  *[Tests](#test)
+  *[Questions](#questions)
 
-   
-  # ${data.github}
-  # ${data.email}
+
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseBadge(data.license)}
+
+
+  ## Questions
+  If you have any question about the repo you can contact me via ${data.email}.
+  You can also find more of my work at[${data.github}](https://github.com/${data.github}).
 
 
 
